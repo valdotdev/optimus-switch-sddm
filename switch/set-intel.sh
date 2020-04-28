9059968218ba83484b2c766b5f67d1b42261a492
val <valnoellopez@gmail.com>
2020-04-29 05:33:40 +0800

    re-added and commented #rm -rf /etc/X11/mhwd.d/99-nvidia.conf and #cp /etc/switch/intel/modeset-xorg.conf /etc/X11/xorg.conf.d/99-intel.conf

--- switch/set-intel.sh
+++ switch/set-intel.sh
@@ -5,6 +5,7 @@
 #with modeset.xorg.conf on the line below.
 echo 'Removing nvidia prime setup......'
 
+#rm -rf /etc/X11/mhwd.d/99-nvidia.conf
 rm -rf /etc/X11/xorg.conf.d/99-nvidia.conf
 rm -rf /etc/modprobe.d/99-nvidia.conf
 rm -rf /etc/modules-load.d/99-nvidia.conf
@@ -13,6 +14,7 @@ rm -rf /usr/share/sddm/scripts/Xsetup
 sleep 1
 echo 'Setting intel only mode.......'
 cp /etc/switch/intel/intel-xorg.conf /etc/X11/xorg.conf.d/99-intel.conf
+#cp /etc/switch/intel/modeset-xorg.conf /etc/X11/xorg.conf.d/99-intel.conf
 cp /etc/switch/intel/intel-modprobe.conf /etc/modprobe.d/99-intel.conf
 cp /etc/switch/intel/no-optimus.sh /usr/share/sddm/scripts/Xsetup
 sleep 1
